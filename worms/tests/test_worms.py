pose creation dict-order fix, wip: scaling fixes

worms/tests/test_worms.py
@@ -1,4 +1,5 @@
 import pytest
+import pickle
 import numpy as np
 from homog import hrot, htrans, axis_angle_of, axis_ang_cen_of
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
@@ -85,6 +86,16 @@ def test_spliceable(c2pose):
     dimer = Spliceable(c2pose, sites=[site1, site2])
     assert dimer.sites[0].resids(dimer) == [1, 2, 3]
     assert dimer.sites[1].resids(dimer) == [13, 14, 15]
+
+
+@pytest.mark.skip  # if('not HAVE_PYROSETTA')
+def test_spliceable_pickle(tmpdir, c2pose):
+    site1 = SpliceSite([1, 2, 3], 'N', 1)
+    site2 = SpliceSite([1, 2, 3], 'N', 2)
+    dimer = Spliceable(c2pose, sites=[site1, site2])
+    pickle.dump(dimer, open(str(os.path.join(tmpdir, 'test.pickle')), 'wb'))
+    dimer2 = pickle.load(open(str(os.path.join(tmpdir, 'test.pickle')), 'rb'))
+    assert str(dimer) == str(dimer2)
 
 
 def test_geom_check():
@@ -433,13 +444,19 @@ def residue_coords(p, ir, n=3):
     return np.stack([c.x, c.y, c.z, 1] for c in crd)
 
 
-def residue_sym_err(p, ang, ir, jr, n=1, axis=[0, 0, 1]):
+def residue_sym_err(p, ang, ir, jr, n=1, axis=[0, 0, 1], verbose=0):
     mxdist = 0
     for i in range(n):
         xyz0 = residue_coords(p, ir + i)
         xyz1 = residue_coords(p, jr + i)
         xyz3 = hrot(axis, ang) @ xyz1.T
         xyz4 = hrot(axis, -ang) @ xyz1.T
+        if verbose:
+            print(i, xyz0)
+            print(i, xyz1)
+            print(i, xyz3.T)
+            print(i, xyz4.T)
+            print()
         mxdist = max(mxdist, min(
             np.max(np.sum((xyz0 - xyz3.T)**2, axis=1)),
             np.max(np.sum((xyz0 - xyz4.T)**2, axis=1))))
@@ -726,10 +743,22 @@ def test_oct(c2pose, c3pose, c4pose, c1pose):
                 [Segment([dimer], entry='N')])
     w = grow(segments, Octahedral(c2=-1, c4=0), thresh=1)
     assert len(w) == 5
+    assert np.allclose(w.indices, np.array([[0, 1, 1, 2, 0, 2, 1],
+                                            [1, 0, 2, 3, 1, 0, 1],
+                                            [1, 0, 0, 0, 3, 2, 0],
+                                            [0, 2, 0, 0, 1, 2, 0],
+                                            [1, 1, 2, 1, 1, 2, 0]]))
     p = w.pose(0, only_connected=0)
+    assert p.sequence() == ('AIAAALAAIAAIAAALAAIAAIAAALAAIAAIAAALAAAAAAAAAAGA'
+                            + 'AAAAAAAAGAAAAAAAAAGAAAAAAAAAAGAAAAAAAAGAATAFLA'
+                            + 'AIPAINYTAFLAAIPAIN')
     assert util.no_overlapping_residues(p)
-    assert 1 > residue_sym_err(p, 90, 1, 31, 6, axis=[1, 0, 0])
-    assert 1 > residue_sym_err(p, 180, 92, 104, 6, axis=[1, 1, 0])
+    # from socket import gethostname
+    # p.dump_pdb(gethostname() + '.pdb')
+    # assert np.allclose(p.residue(1).xyz('CA')[0], 33.0786722948)
+    assert 1 > residue_sym_err(p, 90, 1, 31, 6, axis=[1, 0, 0], verbose=0)
+    assert 1 > residue_sym_err(p, 180, 92, 104, 6, axis=[1, 1, 0], verbose=0)
+    # assert 0
 
 
 @pytest.mark.skipif('not HAVE_PYROSETTA')
@@ -792,7 +821,7 @@ def test_chunk_speed(c2pose, c3pose, c1pose):
     w1 = grow(segments, Octahedral(c3=-1, c2=0), thresh=1, memsize=0)
     t1 = time.time() - t1
     t2 = time.time()
-    w2 = grow(segments, Octahedral(c3=-1, c2=0), thresh=1, memsize=1e9)
+    w2 = grow(segments, Octahedral(c3=-1, c2=0), thresh=1, memsize=1e7)
     t2 = time.time() - t2
 
     print('chunksize', w1.detail['chunksize'], 'time', t1)

worms/worms.py
@@ -1,13 +1,16 @@
 import multiprocessing
 import os
 import itertools as it
+import functools as ft
+import operator
 from collections.abc import Iterable
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 import numpy as np
 from numpy.linalg import inv
 from .criteria import CriteriaList
 try:
+    import pyrosetta
     from pyrosetta import rosetta as ros
     from pyrosetta.rosetta.core import scoring
     rm_lower_t = ros.core.pose.remove_lower_terminus_type_from_pose_residue
@@ -147,6 +150,15 @@ class Spliceable:
         return ('Spliceable: body=(' + str(len(self.body)) + ',' +
                 str(self.body).splitlines()[0].split('/')[-1] +
                 '), sites=' + str([(s.resids(self), s.polarity) for s in self.sites]))
+
+    # def __getstate__(self):
+        # pdbfname = self.body.pdb_info().name() if self.body else None
+        # return (pdbfname, self.sites,
+        # self.bodyid, self.min_seg_len)
+
+    # def __setstate__(self, state):
+        # body = pyrosetta.pose_from_file(state[0]) if state[0] else None
+        # self.__init__(body, state[1], bodyid=state[2], min_seg_len=state[3])
 
 
 class AnnoPose:
@@ -326,7 +338,7 @@ class Segment:
         if ch_en: ir_en -= spliceable.start_of_chain[ch_en]
         if ch_ex: ir_ex -= spliceable.start_of_chain[ch_ex]
         assert ch_en or ch_ex
-        rest = {}
+        rest = OrderedDict()
         did_cyclictrim_in_rest = False
         for i in range(1, len(chains) + 1):
             pchain = chains[i]
@@ -342,15 +354,6 @@ class Segment:
                                        cyclic_entry[i])
         if cyclictrim and iseg == cyclictrim[0]:
             assert cyclictrim_in_rest == did_cyclictrim_in_rest
-        for ap in rest.values():
-            s1 = str(ap.pose.sequence())
-            s2 = str(ap.srcpose.sequence()[ap.src_lb - 1:ap.src_ub])
-            if s1 != s2:
-                print('WARNING: sequence mismatch in "rest", maybe OK, but '
-                      'proceed with caution and tell will to fix!')
-                # print(s1)
-                # print(s2)
-            # assert s1 == s2
         if ch_en: del rest[chains[ch_en]]
         if ch_en == ch_ex:
             assert len(rest) + 1 == len(chains)
@@ -381,6 +384,15 @@ class Segment:
                 assert p.sequence() == pose.sequence()[lb - 1:ub]
             enex = p_en + list(rest.values()) + p_ex
             rest = []
+        for ap in rest:
+            s1 = str(ap.pose.sequence())
+            s2 = str(ap.srcpose.sequence()[ap.src_lb - 1:ap.src_ub])
+            if s1 != s2:
+                print('WARNING: sequence mismatch in "rest", maybe OK, but '
+                      'proceed with caution and tell will to fix!')
+                # print(s1)
+                # print(s2)
+            assert s1 == s2
         if position is not None:
             position = util.rosetta_stub_from_numpy_stub(position)
             for x in enex: x.pose = x.pose.clone()
@@ -792,6 +804,10 @@ def _check_topology(segments, criteria, expert=False):
     return matchlast
 
 
+def bigprod(iterable):
+    return ft.reduce(operator.mul, iterable, 1)
+
+
 def grow(
     segments,
     criteria,
@@ -805,7 +821,7 @@ def grow(
     verbosity=0,
     jobmult=None,
     chunklim=None,
-    max_samples=int(1e24),
+    max_samples=int(1e12),
     max_results=int(1e4)
 ):
     os.environ['OMP_NUM_THREADS'] = '1'
@@ -840,9 +856,9 @@ def grow(
     if max_workers is None: max_workers = util.cpu_count()
     sizes = [len(s.bodyid) for s in segments]
     end = len(segments) - 1
-    while end > 1 and (np.prod(sizes[end:]) < max_workers or
-                       memsize <= 64 * np.prod(sizes[:end])): end -= 1
-    ntot, chunksize, nchunks = (np.product(x)
+    while end > 1 and (bigprod(sizes[end:]) < max_workers or
+                       memsize <= 64 * bigprod(sizes[:end])): end -= 1
+    ntot, chunksize, nchunks = (bigprod(x)
                                 for x in (sizes, sizes[:end], sizes[end:]))
     if max_samples is not None:
         max_samples = np.clip(chunksize * max_workers, max_samples, ntot)
@@ -861,6 +877,14 @@ def grow(
             int(ntot / njob), int(nchunks / njob), sizes, every_other))
         print('max_samples: {:,} max_results: {:,}'.format(
             max_samples, max_results))
+
+    if every_other >= 2**63 or njob > 1e9 or nchunks >= 2**63:
+        print('too big?!?')
+        print('    njob', njob)
+        print('    nchunks', nchunks, nchunks / 2**63)
+        print('    every_other', every_other, every_other / 2**63)
+        return
+
     # run the stuff
     accumulator = util.WormsAccumulator(
         max_results=max_results, max_tmp_size=1e5)