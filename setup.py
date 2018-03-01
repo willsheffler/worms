0e8b1ad16eb142a8eda7f4243d5eb9dfd05d6dd4
Will Sheffler <willsheffler@gmail.com>
2018-02-28 16:25:21 -0800

    Bump version: 0.1.13 → 0.1.14

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ test_requirements = [
 
 setup(
     name='worms',
-    version='0.1.13',
+    version='0.1.14',
     description="Protion Origami via Genetic Fusions",
     long_description=readme + '\n\n' + history,
     author="Will Sheffler",
--- worms/__init__.py
+++ worms/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Will Sheffler"""
 __email__ = 'willsheffler@gmail.com'
-__version__ = '0.1.13'
+__version__ = '0.1.14'
 
 from .worms import *
 from .criteria import *
