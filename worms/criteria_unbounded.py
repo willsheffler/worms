c9ca7f23825aeca5fe2ce8ae8368ebac337300fb
natteruw <natteruw@uw.edu>
2018-04-20 14:50:43 -0700

    we thnk we finished adding p3, p4, p6 stuff

--- worms/criteria_unbounded.py
+++ worms/criteria_unbounded.py
@@ -58,23 +58,35 @@ class AxesAngle(WormCriteria): ## for 2D arrays (maybe 3D in the future?)
         ax2 = segpos[self.to_seg][..., :, 2]  
         if hm.angle(ax1, ax2) > np.pi / 2: ## make sure to align with smaller axis choice
             ax2 = -ax2
-        x = hm.align_vectors(ax1, ax2, self.tgtaxis1, self.tgtaxis2) ## utility function that tries to align ax1 and ax2 to the target axes
-        x[..., :, 3] = - x @ cen1 ## move from_seg cen1 to origin
+        if abs(hm.angle(self.tgtaxis1, self.tgtaxis2)) < 0.1 :
+            d = hm.proj_perp(ax1, cen2 - cen1) #vector delta between cen2 and cen1
+            x = hm.align_vectors(ax1, d, self.tgtaxis1, [0,1,0,0] ) #align d to Y axis
+            x[..., :, 3] = - x @ cen1
+        else:
+            x = hm.align_vectors(ax1, ax2, self.tgtaxis1, self.tgtaxis2) ## utility function that tries to align ax1 and ax2 to the target axes
+            x[..., :, 3] = - x @ cen1 ## move from_seg cen1 to origin
+            x[..., 2, 3] = - (x @ cen2)[...,2] ##move cen2 down along z, such that it's centered at z=0
         return x
+
+    def symfile_modifiers(self, segpos):
+        x = self.alignment(segpos)
+        cen2 = segpos[self.to_seg][..., :, 3]
+        cell_spacing = (x @ cen2)[..., 1] ## setting cell_spacing equal to offset of dimer from origin
+        return dict(scale_positions = cell_spacing)
 
 def Sheet_P321(c3=None, c2=None, **kw):
     if c3 is None or c2 is None:
         raise ValueError('must specify ...?') #one or two of c3, c2
-    return AxesAngle('Sheet_P321', Uz, Ux, from_seg=c3, to_seg=c2, **kw) ##this is currently identical to the D3 format...how do we change it to make it an array?
+    return AxesAngle('Sheet_P321_C3_C2_depth3_1comp', Uz, Ux, from_seg=c3, to_seg=c2, **kw) ##this is currently identical to the D3 format...how do we change it to make it an array?
 
 def Sheet_P4212(c4=None, c2=None, **kw): ##should there be options for multiple C2's?
     if c4 is None or c2 is None:
         raise ValueError('must specify ...?') #one or two of c4, c2
-    return AxesAngle('Sheet_P4212', Uz, Uz, from_seg=c4, to_seg=c2, **kw)
+    return AxesAngle('Sheet_P4212_C4_C2_depth3_1comp', Uz, Ux, from_seg=c4, to_seg=c2, **kw)
 
-def Sheet_P622(c6=None, c2=None, **kw): ##should there be options for multiple C2's?
+def Sheet_P6(c6=None, c2=None, **kw): ##should there be options for multiple C2's?
     if c6 is None or c2 is None:
         raise ValueError('must specify ...?') #one or two of c6, c2
-    return AxesAngle('Sheet_P622', Uz, Ux, from_seg=c6, to_seg=c2, **kw)
+    return AxesAngle('Sheet_P6_C6_C2_depth3_1comp', Uz, Uz, from_seg=c6, to_seg=c2, **kw)
 
 ##def Crystal_example():
--- worms/tests/test_una2d.py
+++ worms/tests/test_una2d.py
@@ -22,13 +22,60 @@ except ImportError:
 only_if_pyrosetta = pytest.mark.skipif('not HAVE_PYROSETTA')
 
 @only_if_pyrosetta
+def test_sheet_P6(c2pose, c6pose, c1pose):
+    helix = Spliceable(c1pose, [(':1', 'N'), ('-7:', 'C')])
+    dimer = Spliceable(c2pose, sites=[('1,:2', 'N'), ('1,-1:', 'C'),])
+    hexamer = Spliceable(c6pose, sites=[('1,:1', 'N'), ('1,-2:', 'C'),])
+    segments = [Segment([hexamer], '_C'),
+                Segment([helix], 'NC'),
+                Segment([helix], 'NC'),
+                Segment([helix], 'NC'),
+                Segment([helix], 'NC'),
+                Segment([helix], 'NC'),
+                Segment([dimer], 'N_')]
+    w = grow(segments, Sheet_P6(c2=-1, c6=0), thresh=1)
+    assert len(w) > 0
+    # print(w.scores)
+    # vis.show_with_z_axes(w, 0)
+    for i in range(len(w)):
+
+        p = w.pose(i, only_connected=0)
+        q,s = w.sympose(i, score=True )
+        print(i,s)
+        if s < 100:
+        #p.dump_pdb('p.pdb')
+            q.dump_pdb('P6_%i_symm.pdb'%i)
+            p.dump_pdb('P6_%i_asymm.pdb'%i)
+
+    assert util.no_overlapping_residues(p)
+    assert 0
+@only_if_pyrosetta
+def test_sheet_P4212(c2pose, c4pose, c1pose):
+    helix = Spliceable(c1pose, [(':4', 'N'), ('-4:', 'C')])
+    dimer = Spliceable(c2pose, sites=[('1,:2', 'N'), ('1,-1:', 'C')])
+    tetramer = Spliceable(c4pose, sites=[('1,:1', 'N'), ('1,-2:', 'C')])
+    segments = [Segment([tetramer], '_C'),
+                Segment([helix], 'NC'),
+                Segment([helix], 'NC'),
+                Segment([helix], 'NC'),
+                Segment([dimer], 'N_')]
+    w = grow(segments, Sheet_P4212(c2=-1, c4=0), thresh=1)
+
+    # print(w.scores)
+    # vis.show_with_z_axes(w, 0)
+    p = w.pose(0, only_connected=0)
+    q = w.sympose(0, )
+    #p.dump_pdb('p.pdb')
+    q.dump_pdb('P4212_symm.pdb')
+    p.dump_pdb('P4212_asymm.pdb')
+
+    assert util.no_overlapping_residues(p) ## basic check on pose to make sure residues are not on top of each other
+
+@only_if_pyrosetta
 def test_sheet_P321(c2pose, c3pose, c1pose):
     helix = Spliceable(c1pose, [(':4', 'N'), ('-4:', 'C')])
-    dimer = Spliceable(c2pose, sites=[('1,:2', 'N'), ('1,-1:', 'C'),
-                                      ('2,:2', 'N'), ('2,-1:', 'C')])
-    trimer = Spliceable(c3pose, sites=[('1,:1', 'N'), ('1,-2:', 'C'),
-                                       ('2,:2', 'N'), ('2,-2:', 'C'),
-                                       ('3,:1', 'N'), ('3,-2:', 'C')])
+    dimer = Spliceable(c2pose, sites=[('1,:2', 'N'), ('1,-1:', 'C'),])
+    trimer = Spliceable(c3pose, sites=[('1,:1', 'N'), ('1,-2:', 'C'),])
     segments = [Segment([trimer], '_C'),
                 Segment([helix], 'NC'),
                 Segment([helix], 'NC'),
@@ -38,12 +85,12 @@ def test_sheet_P321(c2pose, c3pose, c1pose):
     # print(w.scores)
     # vis.show_with_z_axes(w, 0)
     p = w.pose(0, only_connected=0)
-    q = w.sympose(0)
+    q = w.sympose(0, )
     #p.dump_pdb('p.pdb')
-    #q.dump_pdb('q.pdb')
+    q.dump_pdb('P321_symm.pdb')
+    p.dump_pdb('P321_asymm.pdb')
 
     assert util.no_overlapping_residues(p)
-    p.dump_pdb("test.pdb")
-    assert 0
+
     # print(len(p))
 
