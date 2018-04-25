bf7d4ce1854e1c8b08d66d3c581d8ed8fc7be033
Will Sheffler <willsheffler@gmail.com>
2018-04-25 13:26:05 -0700

    Bump version: 0.1.23 → 0.1.24

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='worms',
-    version='0.1.23',
+    version='0.1.24',
     description="Protion Origami via Genetic Fusions",
     long_description=readme + '\n\n' + history,
     author="Will Sheffler",
